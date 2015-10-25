2357df3ca909523d514fa1b9321da505f2f71a24
Ruslan Shaydulin <ruslan@l2sh.ru>
2015-10-25 23:33:00 +0300

    now converts image to proper pdp-11 format

---
+++ WindowsApplication1/Form1.Designer.vb
@@ -0,0 +1,77 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Форма переопределяет dispose для очистки списка компонентов.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Является обязательной для конструктора форм Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'Примечание: следующая процедура является обязательной для конструктора форм Windows Forms
+    'Для ее изменения используйте конструктор форм Windows Form.  
+    'Не изменяйте ее в редакторе исходного кода.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Button1 = New System.Windows.Forms.Button()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Image = Global.WindowsApplication1.My.Resources.Resources.Снимок_экрана_2015_10_10_в_14_59_22
+        Me.PictureBox1.Location = New System.Drawing.Point(12, 12)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(424, 421)
+        Me.PictureBox1.TabIndex = 0
+        Me.PictureBox1.TabStop = False
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(565, 170)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(39, 13)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "Label1"
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(501, 222)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(179, 23)
+        Me.Button1.TabIndex = 2
+        Me.Button1.Text = "Convert Binary to PDP-11 format"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(741, 445)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Button1 As Button
+End Class
---
+++ WindowsApplication1/Form1.vb
@@ -0,0 +1,53 @@
+﻿Public Class Form1
+    Private Sub PictureBox1_Click(sender As Object, e As EventArgs) Handles PictureBox1.Click
+        Dim MyBitmap As New Bitmap(PictureBox1.Image)
+        Dim MyColor As Color
+        Dim MyShadeOfGrey As Integer
+        Dim file As System.IO.StreamWriter
+        file = My.Computer.FileSystem.OpenTextFileWriter("\\Mac\Home\Desktop\test.txt", True)
+
+        For x As Integer = 0 To MyBitmap.Size.Width - 1
+            For y As Integer = 0 To MyBitmap.Size.Height - 1
+                MyColor = MyBitmap.GetPixel(x, y)
+                Dim R As Integer = MyColor.R
+                Dim B As Integer = MyColor.B
+                Dim G As Integer = MyColor.G
+                If (R + G + B) > 300 Then
+                    MyShadeOfGrey = 0
+                    file.Write(1)
+                Else
+                    MyShadeOfGrey = 255
+                    file.Write(0)
+                End If
+                MyColor = Color.FromArgb(MyShadeOfGrey, MyShadeOfGrey, MyShadeOfGrey)
+                MyBitmap.SetPixel(x, y, MyColor)
+            Next
+        Next
+        'Label1.Text = MyColor.Name
+        PictureBox1.Image = MyBitmap
+        PictureBox1.Update()
+        file.Close()
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim file As String
+        Dim counter As Integer = 8
+        Dim tmpNumber As Integer = 0
+        Dim output As System.IO.StreamWriter
+        output = My.Computer.FileSystem.OpenTextFileWriter("\\Mac\Home\Desktop\testHex.txt", True)
+        file = My.Computer.FileSystem.ReadAllText("\\Mac\Home\Desktop\test.txt")
+        For Each c As Char In file
+            counter -= 1
+            If c = "1" Then
+                tmpNumber += (2 ^ counter)
+            End If
+            'output.WriteLine("Tmp num is " + tmpNumber.ToString + "counter " + counter.ToString)
+            If counter = 0 Then
+                output.WriteLine(Hex(tmpNumber))
+                tmpNumber = 0
+                counter = 8
+            End If
+        Next
+        output.Close()
+    End Sub
+End Class
